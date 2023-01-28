ca1939ce6bcf56dee1424a609f7bce36dc67e5e3
Cernean <ccernean@yahoo.com>
2023-01-28 11:02:38 -0500

    CSS Completed

---
+++ script.js
@@ -0,0 +1,13 @@
+const square = document.querySelectorAll(".square");
+const resultText = document.querySelector("#resultText");
+const replayBtn = document.querySelector("#replayBtn");
+const winCondition = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
